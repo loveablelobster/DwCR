d198537d18afdab3a59ef3f46696df0c472d2103
Martin Stein <loveablelobster@fastmail.fm>
2017-12-14 18:02:57 +0100

    slight changes

--- spec/meta_parser_spec.rb
+++ spec/meta_parser_spec.rb
@@ -6,7 +6,7 @@ require_relative '../lib/meta_parser'
 
 #
 module DwCR
-  RSpec.describe 'Meta Parser Methods' do
+  RSpec.describe 'Methods that parse the meta xml and return' do
     let(:meta) do
       xml = <<-HEREDOC
 <?xml version="1.0" ?>
@@ -36,232 +36,238 @@ HEREDOC
       Nokogiri::XML(xml)
     end
 
-    context 'pareses the xml and returns' do
-      it 'an array of hashes:' do
-      	expect(DwCR.parse_meta(meta).size).to be 2
-      	expect(DwCR.parse_meta(meta).map(&:class)).to contain_exactly(Hash, Hash)
+    it 'an array of hashes:' do
+      expect(DwCR.parse_meta(meta).size).to be 2
+      expect(DwCR.parse_meta(meta).map(&:class)).to contain_exactly(Hash, Hash)
+    end
+
+    context 'the core' do
+      it 'has the `is_core` flag set' do
+        expect(DwCR.parse_meta(meta).first).to include :is_core => true
       end
 
-      context 'the core' do
-        it 'has the `is_core` flag set' do
-          expect(DwCR.parse_meta(meta).first).to include :is_core => true
+      it 'has a pluralized `name`' do
+        expect(DwCR.parse_meta(meta).first).to include :name => 'occurrence'.pluralize
+      end
+
+      it 'has a URL defining the `term`' do
+        expect(DwCR.parse_meta(meta).first).to include :term => 'http://rs.tdwg.org/dwc/terms/Occurrence'
+      end
+
+      it 'has the index of the `key_column`' do
+        expect(DwCR.parse_meta(meta).first).to include :key_column => 0
+      end
+
+      context 'has the definitions for `fields`' do
+        it 'has an array of hashes' do
+          expect(DwCR.parse_meta(meta).first[:fields].size).to be 3
+          expect(DwCR.parse_meta(meta)
+                     .first[:fields]
+                     .map(&:class)).to contain_exactly(Hash, Hash, Hash)
         end
 
-        it 'has a pluralized `name`' do
-        	expect(DwCR.parse_meta(meta).first).to include :name => 'occurrence'.pluralize
-        end
+        context 'each field' do
+          it 'has a `term`' do
+            expect(DwCR.parse_meta(meta)
+                       .first[:fields]
+                       .map { |f| f[:term] }).to contain_exactly('http://rs.tdwg.org/dwc/terms/occurrenceID',
+                                                                 'http://rs.tdwg.org/dwc/terms/catalogNumber',
+                                                                 'http://rs.tdwg.org/dwc/terms/institutionCode')
+          end
 
-        it 'has a URL defining the `term`' do
-        	expect(DwCR.parse_meta(meta).first).to include :term => 'http://rs.tdwg.org/dwc/terms/Occurrence'
-        end
+          it 'has a `name`' do
+            expect(DwCR.parse_meta(meta)
+                       .first[:fields]
+                       .map { |f| f[:name] }).to contain_exactly('occurrence_id',
+                                                                 'catalog_number',
+                                                                 'institution_code')
+          end
 
-        it 'has the index of the `key_column`' do
-        	expect(DwCR.parse_meta(meta).first).to include :key_column => 0
-        end
+          it 'has an `alt_name` identical to the name' do
+            expect(DwCR.parse_meta(meta)
+                       .first[:fields]
+                       .map { |f| f[:alt_name] }).to eq(DwCR.parse_meta(meta)
+                                                            .first[:fields]
+                                                            .map { |f| f[:name] })
+          end
 
-        context 'has the definitions for `fields`' do
-        	it 'has an array of hashes' do
-        		expect(DwCR.parse_meta(meta).first[:fields].size).to be 3
-        		expect(DwCR.parse_meta(meta)
-        		           .first[:fields]
-        		           .map(&:class)).to contain_exactly(Hash, Hash, Hash)
-        	end
-
-        	context 'each field' do
-        	  it 'has a `term`' do
+          context 'has an `index`' do
+            it 'with an integer of the column in the source file(s)' do
               expect(DwCR.parse_meta(meta)
-        		             .first[:fields]
-        		             .map { |f| f[:term] }).to contain_exactly('http://rs.tdwg.org/dwc/terms/occurrenceID',
-        		                                                       'http://rs.tdwg.org/dwc/terms/catalogNumber',
-        		                                                       'http://rs.tdwg.org/dwc/terms/institutionCode')
-        	  end
+                         .first[:fields][0...2]
+                         .map { |f| f[:index] }).to eq([0, 1])
+            end
 
-            it 'has a `name`' do
+            it 'or nil if the field is not in the source file(s)' do
               expect(DwCR.parse_meta(meta)
-        		             .first[:fields]
-        		             .map { |f| f[:name] }).to contain_exactly('occurrence_id',
-        		                                                       'catalog_number',
-        		                                                       'institution_code')
+                         .first[:fields]
+                         .last[:index]).to be_nil
             end
+          end
 
-            it 'has an `alt_name` identical to the name' do
+          context 'has a `default`' do
+            it 'with a default value for the column' do
               expect(DwCR.parse_meta(meta)
-        		             .first[:fields]
-        		             .map { |f| f[:alt_name] }).to eq(DwCR.parse_meta(meta)
-        		                                                  .first[:fields]
-        		                                                  .map { |f| f[:name] })
+                         .first[:fields]
+                         .last[:default]).to eq('NHMD')
             end
 
-            context 'has an `index`' do
-              it 'with an integer of the column in the source file(s)' do
-                expect(DwCR.parse_meta(meta)
-                           .first[:fields][0...2]
-                           .map { |f| f[:index] }).to eq([0, 1])
-        		  end
+            it 'or nil if the field does not have a default value' do
+              expect(DwCR.parse_meta(meta)
+                         .first[:fields][0...2]
+                         .map { |f| f[:default] }).to contain_exactly(nil, nil)
+            end
+          end
 
-        		  it 'or nil if the field is not in the source file(s)' do
-                expect(DwCR.parse_meta(meta)
-                           .first[:fields]
-                           .last[:index]).to be_nil
-        		  end
+          context 'has a boolean `has_index` flag' do
+            it 'that is false for every field except the `key_column`' do
+              expect(DwCR.parse_meta(meta)
+                         .first[:fields][1..2]
+                         .map { |f| f[:has_index] }).to contain_exactly(false, false)
             end
 
-            context 'has a `default`' do
-              it 'with a default value for the column' do
-                expect(DwCR.parse_meta(meta)
-                           .first[:fields]
-                           .last[:default]).to eq('NHMD')
-              end
-
-              it 'or nil if the field does not have a default value' do
-                expect(DwCR.parse_meta(meta)
-                           .first[:fields][0...2]
-                           .map { |f| f[:default] }).to contain_exactly(nil, nil)
-              end
+            it 'that is true for the `key_column`' do
+              expect(DwCR.parse_meta(meta)
+                         .first[:fields]
+                         .first[:has_index]).to be_truthy
             end
+          end
 
-            context 'has a boolean `has_index` flag' do
-              it 'that is false for every field except the `key_column`' do
-                expect(DwCR.parse_meta(meta)
+          context 'has a boolean `is_unique` flag' do
+            it 'that is false for every field except the `key_column`' do
+              expect(DwCR.parse_meta(meta)
                            .first[:fields][1..2]
-                           .map { |f| f[:has_index] }).to contain_exactly(false, false)
-              end
-
-              it 'that is true for the `key_column`' do
-              	expect(DwCR.parse_meta(meta)
-                           .first[:fields]
-                           .first[:has_index]).to be_truthy
-              end
+                           .map { |f| f[:is_unique] }).to contain_exactly(false, false)
             end
 
-            context 'has a boolean `is_unique` flag' do
-              it 'that is false for every field except the `key_column`' do
-                expect(DwCR.parse_meta(meta)
-                             .first[:fields][1..2]
-                             .map { |f| f[:is_unique] }).to contain_exactly(false, false)
-              end
-
-              it 'that is true for the `key_column`' do
-              	expect(DwCR.parse_meta(meta)
-                           .first[:fields]
-                           .first[:is_unique]).to be_truthy
-              end
+            it 'that is true for the `key_column`' do
+              expect(DwCR.parse_meta(meta)
+                         .first[:fields]
+                         .first[:is_unique]).to be_truthy
             end
-        	end
+          end
         end
       end
 
-      context 'one extension' do
-        it 'has the `is_core` flag not set' do
-          expect(DwCR.parse_meta(meta).last).to include :is_core => false
+      context 'has the information on the content_files' do
+
+      end
+    end
+
+    context 'one extension' do
+      it 'has the `is_core` flag not set' do
+        expect(DwCR.parse_meta(meta).last).to include :is_core => false
+      end
+
+      it 'has a pluralized `name`' do
+        expect(DwCR.parse_meta(meta).last).to include :name => 'multimedia'.pluralize
+      end
+
+      it 'has a URL defining the `term`' do
+        expect(DwCR.parse_meta(meta).last).to include :term => 'http://rs.tdwg.org/ac/terms/Multimedia'
+      end
+
+      it 'has the index of the `key_column`' do
+        expect(DwCR.parse_meta(meta).last).to include :key_column => 0
+      end
+
+      context 'has the definitions for `fields`' do
+        it 'has an array of hashes' do
+          expect(DwCR.parse_meta(meta).last[:fields].size).to be 5
+          expect(DwCR.parse_meta(meta)
+                     .last[:fields]
+                     .map(&:class)).to contain_exactly(Hash, Hash, Hash, Hash, Hash)
         end
 
-        it 'has a pluralized `name`' do
-        	expect(DwCR.parse_meta(meta).last).to include :name => 'multimedia'.pluralize
-        end
+        context 'each field' do
+          it 'has a `term`' do
+            expect(DwCR.parse_meta(meta)
+                       .last[:fields]
+                       .map { |f| f[:term] }).to contain_exactly(nil,
+                                                                 'http://purl.org/dc/terms/identifier',
+                                                                 'http://rs.tdwg.org/ac/terms/accessURI',
+                                                                 'http://purl.org/dc/terms/rights',
+                                                                 'http://purl.org/dc/elements/1.1/rights')
+          end
 
-        it 'has a URL defining the `term`' do
-        	expect(DwCR.parse_meta(meta).last).to include :term => 'http://rs.tdwg.org/ac/terms/Multimedia'
-        end
+          it 'has a `name`' do
+            expect(DwCR.parse_meta(meta)
+                       .last[:fields]
+                       .map { |f| f[:name] }).to contain_exactly('coreid',
+                                                                 'identifier',
+                                                                 'access_uri',
+                                                                 'rights',
+                                                                 'rights')
+          end
 
-        it 'has the index of the `key_column`' do
-        	expect(DwCR.parse_meta(meta).last).to include :key_column => 0
-        end
-
-        context 'has the definitions for `fields`' do
-        	it 'has an array of hashes' do
-        		expect(DwCR.parse_meta(meta).last[:fields].size).to be 5
-        		expect(DwCR.parse_meta(meta)
-        		           .last[:fields]
-        		           .map(&:class)).to contain_exactly(Hash, Hash, Hash, Hash, Hash)
-        	end
-
-        	context 'each field' do
-        	  it 'has a `term`' do
+          context 'has an `alt_name`' do
+            it 'that is identical to the name where unequivocal' do
               expect(DwCR.parse_meta(meta)
-        		             .last[:fields]
-        		             .map { |f| f[:term] }).to contain_exactly(nil,
-        		                                                       'http://purl.org/dc/terms/identifier',
-        		                                                       'http://rs.tdwg.org/ac/terms/accessURI',
-        		                                                       'http://purl.org/dc/terms/rights',
-        		                                                       'http://purl.org/dc/elements/1.1/rights')
-        	  end
+                         .last[:fields][0...4]
+                         .map { |f| f[:alt_name] }).to eq(DwCR.parse_meta(meta)
+                                                              .last[:fields][0...4]
+                                                              .map { |f| f[:name] })
+            end
 
-            it 'has a `name`' do
+            it 'that is suffixed with `!` where equivocal' do
               expect(DwCR.parse_meta(meta)
-        		             .last[:fields]
-        		             .map { |f| f[:name] }).to contain_exactly('coreid',
-        		                                                       'identifier',
-        		                                                       'access_uri',
-        		                                                       'rights',
-        		                                                       'rights')
+                         .last[:fields]
+                         .last[:alt_name]).to eq('rights!')
             end
+          end
 
-            context 'has an `alt_name`' do
-              it 'that is identical to the name where unequivocal' do
-                expect(DwCR.parse_meta(meta)
-                           .last[:fields][0...4]
-                           .map { |f| f[:alt_name] }).to eq(DwCR.parse_meta(meta)
-                                                                .last[:fields][0...4]
-                                                                .map { |f| f[:name] })
-              end
-
-              it 'that is suffixed with `!` where equivocal' do
-                expect(DwCR.parse_meta(meta)
-                           .last[:fields]
-                           .last[:alt_name]).to eq('rights!')
-              end
-            end
-
-            context 'has an `index`' do
-              it 'with an integer of the column in the source file(s)' do
-                expect(DwCR.parse_meta(meta)
-                           .last[:fields][0...4]
-                           .map { |f| f[:index] }).to eq([0, 1, 2, 6])
-        		  end
-
-        		  it 'or nil if the field is not in the source file(s)' do
-                expect(DwCR.parse_meta(meta)
-                           .last[:fields]
-                           .last[:index]).to be_nil
-        		  end
-            end
-
-            context 'has a `default`' do
-              it 'with a default value for the column' do
-                expect(DwCR.parse_meta(meta)
-                           .last[:fields][3...5]
-                           .map { |f| f[:default] }).to contain_exactly('© 2008 XY Museum', 'http://creativecommons.org/licenses/by/4.0/deed.en_US')
-              end
-
-              it 'or nil if the field does not have a default value' do
-                expect(DwCR.parse_meta(meta)
-                           .last[:fields][0...3]
-                           .map { |f| f[:default] }).to eq([nil, nil, nil])
-              end
-            end
-
-            context 'has a boolean `has_index` flag' do
-              it 'that is false for every field except the `key_column`' do
-                expect(DwCR.parse_meta(meta)
-                             .last[:fields][1...5]
-                             .map { |f| f[:has_index] }).to contain_exactly(false, false, false, false)
-              end
-
-              it 'that is true for the `key_column`' do
-              	expect(DwCR.parse_meta(meta)
-                           .last[:fields]
-                           .first[:has_index]).to be_truthy
-              end
-            end
-
-            it 'has a boolean `is_unique` flag' do
+          context 'has an `index`' do
+            it 'with an integer of the column in the source file(s)' do
               expect(DwCR.parse_meta(meta)
-                           .last[:fields]
-                           .map { |f| f[:is_unique] }).to contain_exactly(false, false, false, false, false)
+                         .last[:fields][0...4]
+                         .map { |f| f[:index] }).to eq([0, 1, 2, 6])
             end
-        	end
+
+            it 'or nil if the field is not in the source file(s)' do
+              expect(DwCR.parse_meta(meta)
+                         .last[:fields]
+                         .last[:index]).to be_nil
+            end
+          end
+
+          context 'has a `default`' do
+            it 'with a default value for the column' do
+              expect(DwCR.parse_meta(meta)
+                         .last[:fields][3...5]
+                         .map { |f| f[:default] }).to contain_exactly('© 2008 XY Museum', 'http://creativecommons.org/licenses/by/4.0/deed.en_US')
+            end
+
+            it 'or nil if the field does not have a default value' do
+              expect(DwCR.parse_meta(meta)
+                         .last[:fields][0...3]
+                         .map { |f| f[:default] }).to eq([nil, nil, nil])
+            end
+          end
+
+          context 'has a boolean `has_index` flag' do
+            it 'that is false for every field except the `key_column`' do
+              expect(DwCR.parse_meta(meta)
+                           .last[:fields][1...5]
+                           .map { |f| f[:has_index] }).to contain_exactly(false, false, false, false)
+            end
+
+            it 'that is true for the `key_column`' do
+              expect(DwCR.parse_meta(meta)
+                         .last[:fields]
+                         .first[:has_index]).to be_truthy
+            end
+          end
+
+          it 'has a boolean `is_unique` flag' do
+            expect(DwCR.parse_meta(meta)
+                         .last[:fields]
+                         .map { |f| f[:is_unique] }).to contain_exactly(false, false, false, false, false)
+          end
         end
+      end
+
+      context 'has the information on the content_files' do
+
       end
     end
   end
