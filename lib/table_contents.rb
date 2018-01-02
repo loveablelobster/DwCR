54c3d66f1ba2a09bd4a2ced9a3c2f0d889b39da8
Martin Stein <loveablelobster@fastmail.fm>
2018-01-02 12:05:17 +0100

    analyze file contents

--- lib/table_contents.rb
+++ lib/table_contents.rb
@@ -3,40 +3,75 @@
 require 'csv'
 require 'set'
 
+# set custom CSV::Converters
+CSV::Converters[:safe_numeric] = lambda do |field|
+  case field.strip
+  when /^-?[0-9]+$/
+    field.to_i
+  when /^-?[0-9]*\.[0-9]+$/
+    field.to_f
+  else
+    field
+  end
+end
+
+CSV::Converters[:blank_to_nil] = lambda do |field|
+  field&.empty? ? nil : field
+end
+
 #
 module DwCR
   def self.analyze(files)
     columns = {}
     files.each do |file|
-      CSV.foreach(file) do |row|
-        row.each_with_index do |cell, index|
+      # read the CSV::Table
+      table = CSV.read(file,
+                       headers: headers(file),
+                       converters: [:blank_to_nil, :safe_numeric, :date] )
 
-          columns[index] ||= { length: 0, type: nil}
-
-          # fill lengths hash
-          max_length = columns[index][:length]
-          cell_length = cell&.length || 0
-          columns[index][:length] = cell_length > max_length ? cell_length : max_length
-
-          # fill content_types hash
-          columns[index][:type] ||= Set.new
-          ctype = case cell.strip # FIXME: also test for dates
-                  when /^[0-9]+$/
-                    :integer
-                  when /^[0-9]*\.[0-9]+$/
-                    :float
-                  else
-                    :string
-                  end
-          columns[index][:type].add? ctype
-
-          # FIXME: when coordinate, can be float or int, or even string if '-'
-          # if set is integer or float -> float
-        end
+      # build columns hash
+      table.by_col!.each do |col|
+        columns[col[0]] ||= []
+        cells = col[1].compact
+        t = collapse(cells.map(&:class).uniq)
+        l = cells.map(&:to_s).map(&:length).max || 0
+        columns[col[0]] << { length: l, type: t }
       end
+
+      # consolidate array of hashes of column info (one hash per file) into single hash
+      columns.each { |k, v| columns[k] = consolidate v }
     end
-#     content_type = types.length > 1 ? :string : types.first
-    p columns
     columns
+  end
+
+#   private
+
+  def self.collapse(types)
+    return types.first if types.size == 1
+    return nil if types.empty? # or String
+    return String if types.include?(String)
+    return Float if types.size == 2 && types.include?(Float) && types.include?(Integer)
+    String
+  end
+
+  # reads the first line of the CSV file
+  # returns the columns indices as an array
+  def self.headers(file)
+    CSV.open(file, &:readline).size.times.collect { |i| i }
+  end
+
+  def self.consolidate(array_of_hashes)
+    return array_of_hashes.first if array_of_hashes.size == 1
+    lengths = array_of_hashes.map { |h| h[:length] }
+    types = array_of_hashes.map { |h| h[:type] }.uniq
+
+    col_type = if types.size == 1
+      types.first
+    elsif types.size == 2 && types.include?(Float) && types.include?(Integer)
+      Float
+    else
+      String
+    end
+    { length: lengths.max, type: col_type }
   end
 end
