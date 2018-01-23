179451432d7b0835ef6094321dfd7424034285a8
martin <loveablelobster@fastmail.fm>
2018-01-23 17:57:27 +0100

    content_file_spec
    examples for
      #file_name
      #content_headers

--- spec/content_file_spec.rb
+++ spec/content_file_spec.rb
@@ -11,54 +11,46 @@ module DwCR
   end
 
   RSpec.describe 'ContentFile' do
-    it 'returns a list of alt_names as content headers, sorted by index' do
-      meta_entity = MetaEntity.create(term: 'example.org/item')
-      meta_entity.add_meta_attribute(name: 'term_b', index: 2)
-      meta_entity.add_meta_attribute(name: 'term_a', index: 1)
-      content_file = meta_entity.add_content_file(name: 'file', path: 'home')
-      expect(content_file.content_headers).to contain_exactly(:term_a, :term_b)
+    it 'returns the full file name including the path' do
+      f = ContentFile.create(name: 'table.csv', path: File.path('/dev/null'))
+      expect(f.file_name).to eq(File.path('/dev/null/table.csv'))
     end
 
-    # example for load
+    context 'returns column names as an array of symbols for names' do
+      let(:h) do
+        e = MetaEntity.create(term: 'example.org/item')
+        e.add_meta_attribute(name: 'term_a', index: 0, type: 'string')
+        e.add_meta_attribute(name: 'term_b', index: 1, type: nil)
+        e.add_meta_attribute(name: 'term_c', index: nil, type: 'string')
+        e.add_meta_attribute(name: 'term_d', index: 2, type: 'string')
+        e.add_meta_attribute(name: 'term_e', index: nil, type: nil)
+        f = e.add_content_file(name: 'table.csv')
+        f.content_headers
+      end
 
-    # example for unload!
+      it 'including attributes that have index and type' do
+        expect(h).to include(:term_a, :term_d)
+      end
 
-    # example for error when extension is loaded before core
+      it 'not including attributes that do not have an index' do
+      	expect(h).not_to include :term_c
+      end
 
-#     it 'skips blank columns in a csv' do
-#       meta_entity = MetaEntity.create(term: 'example.org/item')
-#       meta_entity.add_meta_attribute(name: 'term_a', index: 0, type: 'string')
-#       meta_entity.add_meta_attribute(name: 'term_b', index: 1, type: nil)
-#       meta_entity.add_meta_attribute(name: 'term_c', index: 2, type: 'string')
-#       meta_entity.add_meta_attribute(name: 'term_d', index: 3, type: 'string')
-#       meta_entity.add_meta_attribute(name: 'term_e', index: 4, type: nil)
-#       meta_entity.add_meta_attribute(name: 'term_f', index: 5, type: 'string')
-#       meta_entity.add_meta_attribute(name: 'term_g', index: 6, type: 'string')
-#       meta_entity.add_meta_attribute(name: 'term_h', index: 7, type: 'string')
-#       meta_entity.add_meta_attribute(name: 'term_i', index: 8, type: nil)
-#       meta_entity.add_meta_attribute(name: 'term_j', index: 9, type: 'string')
-#
-#       content_file = meta_entity.add_content_file(name: 'file', path: 'home')
-#       row = [
-#         'value a',
-#         'skip b',
-#         'value c',
-#         'value d',
-#         'skip e',
-#         'value f',
-#         'value g',
-#         'value h',
-#         'skip i',
-#         'value j'
-#         ]
-#       expect(content_file.values_for(row)).to eq({term_a: 'value a',
-#                                                  term_c: 'value c',
-#                                                  term_d: 'value d',
-#                                                  term_f: 'value f',
-#                                                  term_g: 'value g',
-#                                                  term_h: 'value h',
-#                                                  term_j: 'value j'
-#                                                  })
-#     end
+      it 'not including attributes that do not have a type' do
+      	expect(h).not_to include :term_b
+      end
+
+      it 'not including attributes that have neither index nor type' do
+      	expect(h).not_to include :term_e
+      end
+    end
+
+    context 'loads' do
+
+    end
+
+    context 'unloads' do
+
+    end
   end
 end
