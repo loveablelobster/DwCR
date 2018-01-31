7d7c0b4f4db6b108406bc2fb3b2c736cf45b06ac
martin <loveablelobster@fastmail.fm>
2018-01-31 08:41:23 +0100

    consolidated files

--- lib/db/schematables.rb
+++ lib/db/schematables.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'psych'
+require 'sqlite3'
 
 #
 module DwCR
@@ -32,5 +33,34 @@ module DwCR
   def self.add_foreign_key(table, entity)
     return unless entity.core
     table.foreign_key(entity.core.foreign_key, entity.core.table_name)
+  end
+
+  def self.inspect_table(table, method)
+    DB.indexes(table).values.map { |x| x[:columns] }.flatten
+    DB.send(method, table)
+  rescue Sequel::Error
+    false
+  end
+
+  def self.columns?(table, columns)
+    db_cols = inspect_table(table, :schema)
+    return unless db_cols
+    exp_cols = columns.map(&:first).unshift(:id)
+    exp_cols == db_cols.map(&:first)
+  end
+
+  def self.indexes?(table, columns)
+    db_idxs = inspect_table(table, :indexes)
+    return unless db_idxs
+    exp_idxs = columns.select { |column| column[2]&.fetch(:index, false) }
+                      .map(&:first)
+    exp_idxs == db_idxs.values.map { |x| x[:columns] }.flatten
+  end
+
+  def self.metaschema?
+    tabledefs = Psych.load_file('lib/store/metaschema.yml')
+    status = tabledefs.map { |td| columns?(*td) && indexes?(*td) }.uniq
+    return false if status.size > 1
+    status.first
   end
 end
--- lib/dwcr.rb
+++ lib/dwcr.rb
@@ -2,7 +2,6 @@
 
 require 'sequel'
 
-require_relative 'db/connection'
 require_relative 'db/schematables'
 require_relative 'models/dynamic_models'
 
@@ -27,6 +26,21 @@ module DwCR
     DwCR.load_models(archive)
   end
 
+  # Updates all MetaAttribute instances in a MetaArchive
+  # with parameters from files in ContentFile
+  # _schema_options_: a Hash with attribute names as keys and boolean values
+  # <tt>{ :type => true, :length => true }</tt>
+  # updates any attribute given as key where value is _true_
+  def self.update_schema(archive, **options)
+    return if options.empty?
+
+    # FIXME: throw an error if metaschema is not loaded
+    # FIXME: handle situation where schema tables have been created
+    options.select! { |_k, v| v == true }
+    archive.meta_entities
+           .each { |entity| entity.update_meta_attributes!(*options.keys) }
+  end
+
   # Loads the contents of all CSV files associated with an archive
   # into the shema tables
   def self.load_contents_for(archive)
@@ -46,20 +60,5 @@ module DwCR
                            class: entity_model)
       entity_model
     end
-  end
-
-  # Updates all MetaAttribute instances in a MetaArchive
-  # with parameters from files in ContentFile
-  # _schema_options_: a Hash with attribute names as keys and boolean values
-  # <tt>{ :type => true, :length => true }</tt>
-  # updates any attribute given as key where value is _true_
-  def self.update_schema(archive, **options)
-    return if options.empty?
-
-    # FIXME: throw an error if metaschema is not loaded
-    # FIXME: handle situation where schema tables have been created
-    options.select! { |_k, v| v == true }
-    archive.meta_entities
-           .each { |entity| entity.update_meta_attributes!(*options.keys) }
   end
 end
