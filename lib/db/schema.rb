9cc79bda36aba875fb7d51855d6bfd1550d3695c
Martin Stein <loveablelobster@fastmail.fm>
2018-02-01 12:24:31 +0100

    schema_spec.rb

--- lib/db/schema.rb
+++ lib/db/schema.rb
@@ -5,6 +5,9 @@ require 'sqlite3'
 
 #
 module DwCR
+  # Creates the tables for the metaschema present in every DwCR
+  # _meta_archives_, _meta_entities_, _meta_attributes_, _content_files_
+  # loads the Sequel::Model classes for these tables
   def self.create_metaschema
     tabledefs = Psych.load_file(File.join(__dir__, 'metaschema_tables.yml'))
     tabledefs.to_h.each do |table, columns|
@@ -17,6 +20,54 @@ module DwCR
     require_relative '../models/meta_entity'
     require_relative '../models/meta_attribute'
     require_relative '../models/content_file'
+  end
+
+  # Returns schema or index parameters for a table, depending on the
+  # second argument (+:schema+ or +:indexes+)
+  # returns +false+ if the table does not exist
+  def self.inspect_table(table, method)
+    DB.indexes(table).values.map { |x| x[:columns] }.flatten
+    DB.send(method, table)
+  rescue Sequel::Error
+    false
+  end
+
+  # Performs an integrety check on +table+, veryfies all columns
+  # are present with the parameters given in +columns+;
+  # a column parameter is an array with the structure:
+  # <tt>[:column_name, :column_type, {column_options} ]</tt>
+  def self.columns?(table, *columns)
+    db_cols = inspect_table(table, :schema)
+    return unless db_cols
+    exp_cols = columns.map(&:first).unshift(:id)
+    exp_cols == db_cols.map(&:first)
+  end
+
+  # Performs an integrety check on +table+, veryfies all indices
+  # are present with the parameters given in +columns+;
+  # a column parameter is an array with the structure:
+  # <tt>[:column_name, :column_type, {column_options} ]</tt>
+  def self.indexes?(table, *columns)
+    db_idxs = inspect_table(table, :indexes)
+    return unless db_idxs
+    exp_idxs = columns.select { |column| column[2]&.fetch(:index, false) }
+                      .map(&:first)
+    exp_idxs & db_idxs.values.map { |x| x[:columns] }.flatten == exp_idxs
+  end
+
+  # Performs an integrity check on the metaschema in the DWCR file
+  # (the current database connection)
+  # returns true if all tables, columns, and indices as given in
+  # _config/metaschema_tables.yml_ are present
+  def self.metaschema?
+    tabledefs = Psych.load_file('lib/db/metaschema_tables.yml')
+    status = tabledefs.map do |td|
+      table = td.first
+      columns = td.last
+      columns?(table, *columns) && indexes?(table, *columns)
+    end
+    return false if status.uniq.size > 1
+    status.first
   end
 
   def self.create_schema_table(entity)
@@ -33,34 +84,5 @@ module DwCR
   def self.add_foreign_key(table, entity)
     return unless entity.core
     table.foreign_key(entity.core.foreign_key, entity.core.table_name)
-  end
-
-  def self.inspect_table(table, method)
-    DB.indexes(table).values.map { |x| x[:columns] }.flatten
-    DB.send(method, table)
-  rescue Sequel::Error
-    false
-  end
-
-  def self.columns?(table, columns)
-    db_cols = inspect_table(table, :schema)
-    return unless db_cols
-    exp_cols = columns.map(&:first).unshift(:id)
-    exp_cols == db_cols.map(&:first)
-  end
-
-  def self.indexes?(table, columns)
-    db_idxs = inspect_table(table, :indexes)
-    return unless db_idxs
-    exp_idxs = columns.select { |column| column[2]&.fetch(:index, false) }
-                      .map(&:first)
-    exp_idxs == db_idxs.values.map { |x| x[:columns] }.flatten
-  end
-
-  def self.metaschema?
-    tabledefs = Psych.load_file('lib/store/metaschema.yml')
-    status = tabledefs.map { |td| columns?(*td) && indexes?(*td) }.uniq
-    return false if status.size > 1
-    status.first
   end
 end
--- spec/schema_spec.rb
+++ spec/schema_spec.rb
@@ -2,162 +2,300 @@
 
 require_relative '../lib/db/schema'
 
-RSpec.describe DwCR, 'schema methods' do
+RSpec.describe DwCR, '#module methods' do
   context 'when creating the metaschema' do
-    it 'creates the meta_archives table' do pending
-
+    it 'creates the meta_archives table' do
+      expect(DB.table_exists?(:meta_archives)).to be_truthy
     end
 
     context 'when created the meta_archives table' do
-      it 'has a core_id foreign key column' do pending
-#     - !ruby/sym integer
-#     - !ruby/sym index: true
+      let(:columns) { DB.schema(:meta_archives) }
+
+      it 'has a core_id foreign key column (integer)' do
+        expect(columns)
+          .to include a_collection_including(:core_id,
+                                             a_hash_including(type: :integer))
+        # FIXME: test for index: true
       end
 
-      it 'has name column' do pending
-#     - !ruby/sym name
-#     - !ruby/sym string
+      it 'has name column (string)' do
+        expect(columns)
+          .to include a_collection_including(:name,
+                                             a_hash_including(type: :string))
       end
 
-      it 'has a path column' do pending
-#     - !ruby/sym path
-#     - !ruby/sym string
+      it 'has a path column (string)' do
+        expect(columns)
+          .to include a_collection_including(:path,
+                                             a_hash_including(type: :string))
       end
 
-      it 'has a xmlns column' do pending
-#     - !ruby/sym string
-#     - !ruby/sym default: 'http://rs.tdwg.org/dwc/text/'
+      it 'has a xmlns column (string, default:'\
+         ' \'http://rs.tdwg.org/dwc/text/\')' do
+        d = '\'http://rs.tdwg.org/dwc/text/\''
+        expect(columns)
+          .to include a_collection_including(:xmlns,
+                                             a_hash_including(type: :string,
+                                                              default: d))
       end
 
-      it 'has a xmlns__xs column' do pending
-#     - !ruby/sym string
-#     - !ruby/sym default: 'http://www.w3.org/2001/XMLSchema'
+      it 'has a xmlns__xs column (string, default:'\
+         ' \'http://www.w3.org/2001/XMLSchema\')' do
+        d = '\'http://www.w3.org/2001/XMLSchema\''
+        expect(columns)
+          .to include a_collection_including(:xmlns__xs,
+                                             a_hash_including(type: :string,
+                                                              default: d))
       end
 
-      it 'has a xmlns__xsi column' do pending
-#     - !ruby/sym string
-#     - !ruby/sym default: 'http://www.w3.org/2001/XMLSchema-instance'
+      it 'has a xmlns__xsi column (string, default:'\
+         ' \'http://www.w3.org/2001/XMLSchema-instance\')' do
+        d = '\'http://www.w3.org/2001/XMLSchema-instance\''
+        expect(columns)
+          .to include a_collection_including(:xmlns__xsi,
+                                             a_hash_including(type: :string,
+                                                              default: d))
       end
 
-      it 'has a xsi__schema_location column' do pending
-#     - !ruby/sym string
-#     - !ruby/sym default: 'http://rs.tdwg.org/dwc/text/ http://rs.tdwg.org/dwc/text/tdwg_dwc_text.xsd'
+      it 'has a xsi__schema_location column (string, default:'\
+         ' \'http://rs.tdwg.org/dwc/text/'\
+         ' http://rs.tdwg.org/dwc/text/tdwg_dwc_text.xsd\')' do
+        d = '\'http://rs.tdwg.org/dwc/text/'\
+            ' http://rs.tdwg.org/dwc/text/tdwg_dwc_text.xsd\''
+        expect(columns)
+          .to include a_collection_including(:xsi__schema_location,
+                                             a_hash_including(type: :string,
+                                                              default: d))
       end
+    end
+
+    it 'creates the meta_attributes table' do
+      expect(DB.table_exists?(:meta_attributes)).to be_truthy
     end
 
     context 'when creating the meta_attributes table' do
-      it 'has a meta_entity_id foreign key column' do pending
-#     - !ruby/sym integer
-#     - !ruby/sym index: true
+      let(:columns) { DB.schema(:meta_attributes) }
+
+      it 'has a meta_entity_id foreign key column (integer)' do
+        expect(columns)
+          .to include a_collection_including(:meta_entity_id,
+                                             a_hash_including(type: :integer))
+        # FIXME: test for index: true
       end
 
-      it 'has a type column' do pending
-#     - !ruby/sym string
-#     - !ruby/sym default: string
+      it 'has a type column (string, default: \'string\')' do
+        d = '\'string\''
+        expect(columns)
+          .to include a_collection_including(:type,
+                                             a_hash_including(type: :string,
+                                                              default: d))
       end
 
-      it 'has a name column' do pending
-#     - !ruby/sym string
-#       !ruby/sym index: true
-#       !ruby/sym null: false
+      it 'has a name column (string, allow null: false)' do
+        a_n = false
+        expect(columns)
+          .to include a_collection_including(:name,
+                                             a_hash_including(type: :string,
+                                                              allow_null: a_n))
+        # FIXME: test for index: true
       end
 
-      it 'has a term index column' do pending
-#     - !ruby/sym string
+      it 'has a term column (string)' do
+        expect(columns)
+          .to include a_collection_including(:term,
+                                             a_hash_including(type: :string))
       end
 
-      it 'has a default column' do pending
-#     - !ruby/sym string
+      it 'has a default column (string)' do
+        expect(columns)
+          .to include a_collection_including(:default,
+                                             a_hash_including(type: :string))
       end
 
-      it 'has an index column' do pending
-#     - !ruby/sym integer
+      it 'has an index column (integer)' do
+        expect(columns)
+          .to include a_collection_including(:index,
+                                             a_hash_including(type: :integer))
       end
 
-      it 'has a max_content_length column' do pending
-#     - !ruby/sym integer
+      it 'has a max_content_length column (integer)' do
+        expect(columns)
+          .to include a_collection_including(:max_content_length,
+                                             a_hash_including(type: :integer))
       end
+    end
+
+    it 'creates the meta_entities table' do
+      expect(DB.table_exists?(:meta_entities)).to be_truthy
     end
 
     context 'when creating the meta_entities table' do
-      it 'has a meta_archive_id foreign key column' do pending
-#     - !ruby/sym integer
-#     - !ruby/sym index: true
+      let(:columns) { DB.schema(:meta_entities) }
+
+      it 'has a meta_archive_id foreign key column (integer)' do
+        expect(columns)
+          .to include a_collection_including(:meta_archive_id,
+                                             a_hash_including(type: :integer))
+        # FIXME: test for index: true
       end
 
-      it 'has a name column' do pending
-#     - !ruby/sym string
-#     - !ruby/sym null: false
+      it 'has a core_id foreign key column (integer)' do
+        expect(columns)
+          .to include a_collection_including(:core_id,
+                                             a_hash_including(type: :integer))
+        # FIXME: test for index: true
       end
 
-      it 'has a term column' do pending
-#     - !ruby/sym term
-#     - !ruby/sym string
+      it 'has a name column (string, allow null: false)' do
+        a_n = false
+        expect(columns)
+          .to include a_collection_including(:name,
+                                             a_hash_including(type: :string,
+                                                              allow_null: a_n))
       end
 
-      it 'has a is_core column' do pending
-#     - !ruby/sym is_core
-#     - !ruby/sym boolean
+      it 'has a term column (string)' do
+        expect(columns)
+          .to include a_collection_including(:term,
+                                             a_hash_including(type: :string))
       end
 
-      it 'has a key_column column' do pending
-#     - !ruby/sym integer
+      it 'has an is_core column (boolean)' do
+        expect(columns)
+          .to include a_collection_including(:is_core,
+                                             a_hash_including(type: :boolean))
       end
 
-      it 'has a fields_enclosed_by column' do pending
-#     - !ruby/sym string
-#     - !ruby/sym default: '&quot;'
+      it 'has a key_column column (integer)' do
+        expect(columns)
+          .to include a_collection_including(:key_column,
+                                             a_hash_including(type: :integer))
       end
 
-      it 'has a fields_terminated_by column' do pending
-#     - !ruby/sym string
-#     - !ruby/sym default: ','
+      it 'has a fields_enclosed_by column (string, default: \'&quot;\')' do
+        d = '\'&quot;\''
+        expect(columns)
+          .to include a_collection_including(:fields_enclosed_by,
+                                             a_hash_including(type: :string,
+                                                              default: d))
       end
 
-      it 'has a lines_terminated_by column' do pending
-#     - !ruby/sym string
-#     - !ruby/sym default: '\r\n'
+      it 'has a fields_terminated_by column (srting, default: \',\')' do
+        d = '\',\''
+        expect(columns)
+          .to include a_collection_including(:fields_terminated_by,
+                                             a_hash_including(type: :string,
+                                                              default: d))
       end
 
-      it 'has a core_id foreign key column' do pending
-#     - !ruby/sym integer
-#     - !ruby/sym index: true
+      it 'has a lines_terminated_by column (string, default: \'\r\n\')' do
+        d = '\'\r\n\''
+        expect(columns)
+          .to include a_collection_including(:lines_terminated_by,
+                                             a_hash_including(type: :string,
+                                                              default: d))
       end
     end
 
+    it 'creates the content_files table' do
+      expect(DB.table_exists?(:content_files)).to be_truthy
+    end
+
     context 'when creating the content_files table' do
-      it 'has a meta_entity_id foreign key column' do pending
-#     - !ruby/sym integer
-#     - !ruby/sym index: true
+      let(:columns) { DB.schema(:content_files) }
+
+      it 'has a meta_entity_id foreign key column (integer)' do
+        expect(columns)
+          .to include a_collection_including(:meta_entity_id,
+                                             a_hash_including(type: :integer))
+        # FIXME: test for index: true
       end
 
-      it 'has a name column' do pending
-#     - !ruby/sym string
+      it 'has a name column (string, allow null: false)' do
+        a_n = false
+        expect(columns)
+          .to include a_collection_including(:name,
+                                             a_hash_including(type: :string,
+                                                              allow_null: a_n))
       end
 
-      it 'has a path column' do pending
-#     - !ruby/sym string
+      it 'has a path column (string)' do
+        expect(columns)
+          .to include a_collection_including(:path,
+                                             a_hash_including(type: :string))
       end
 
-      it 'has a is_loaded column' do pending
-#     - !ruby/sym boolean
-#     - !ruby/sym default: false
+      it 'has a is_loaded column (boolean, default: false)' do
+        expect(columns)
+          .to include a_collection_including(:is_loaded,
+                                             a_hash_including(type: :boolean,
+                                                              default: '0'))
       end
     end
   end
 
-  # DwCR.create_metaschema
+  context 'when inspecting a table' do
+    it 'fetches the schema for the table when passed :schema' do
+      expect(DwCR.inspect_table(:content_files, :schema))
+        .to include a_collection_including(:name),
+                    a_collection_including(:path),
+                    a_collection_including(:is_loaded)
+    end
 
-  # DwCR.create_schema_table(entity)
+    it 'fetches the indexes for the table when passed :indexes' do
+      expect(DwCR.inspect_table(:content_files, :indexes))
+        .to include :content_files_meta_entity_id_index
+    end
 
-  # DwCR.add_foreign_key(table, entity)
+    it 'returns false when the table does not exist' do
+      expect(DwCR.inspect_table(:not_a_table, :schema)).to be_falsey
+    end
+  end
 
-  # DwCR.inspect_table(table, method)
+  context 'when verifying metaschema integrity' do
+    let :content_files_schema do
+      yml = File.join(File.expand_path(Dir.pwd), '/lib/db/metaschema_tables.yml')
+      Psych.load_file(File.path(yml))[:content_files]
+    end
 
-  # DwCR.columns?(table, columns)
+    let :incomplete_schema do
+      [
+        [:path, :string],
+        [:is_loaded, :boolean, { default: false }]
+      ]
+    end
 
-  # DWCR.indexes?(table, columns)
+    it 'it returns true if the metaschema has all tables, columns,'\
+       ' and indices defined in config/metaschema_tables.yml' do
+      expect(DwCR.metaschema?).to be_truthy
+    end
 
-  # DwCR.metaschema?
+    context 'when inspecting columns' do
+      it 'returns true if the table has all columns defined in the yml' do
+        expect(DwCR::columns?(:content_files, *content_files_schema)).to be_truthy
+      end
 
+      it 'returns false if the table is missing a column defined in the yml' do
+        expect(DwCR::columns?(:content_files, *incomplete_schema)).to be_falsey
+      end
+    end
+
+    context 'when inspecting indexes' do
+      it 'returns true if the table has all columns defined in the yml' do
+        expect(DwCR::indexes?(:content_files, *content_files_schema)).to be_truthy
+      end
+
+      it 'returns false if the table is missing a column defined in the yml' do
+        expect(DwCR::columns?(:content_files, *incomplete_schema)).to be_falsey
+      end
+    end
+  end
+
+  context 'when creating a DwCA schema table' do
+    # DwCR.create_schema_table(entity)
+  end
+
+  context 'when adding a foreign key' do
+    # DwCR.add_foreign_key(table, entity)
+  end
 end
