7d6026aa1b1fa0f2301697513cf89d1f791a7734
Martin Stein <loveablelobster@fastmail.fm>
2018-05-14 12:33:21 +0200

    more inflections

--- lib/dwcr/inflections.rb
+++ lib/dwcr/inflections.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 Sequel.inflections do |inflect|
-  inflect.uncountable 'multimedia'
-  inflect.uncountable(/media$/)
+#   inflect.uncountable 'multimedia'
+#   inflect.uncountable 'media'
+  inflect.singular(/(media)$/i, '\1')
+  inflect.plural(/(media)$/i, '\1')
+  inflect.irregular 'taxon', 'taxa'
+  inflect.uncountable 'metadata'
 end
 
 String.inflections do |inflect|
-  inflect.uncountable 'multimedia'
-  inflect.uncountable(/media$/)
+#   inflect.uncountable 'multimedia'
+#   inflect.uncountable 'media'
+  inflect.singular(/(media)$/i, '\1')
+  inflect.plural(/(media)$/i, '\1')
+  inflect.irregular 'taxon', 'taxa'
+  inflect.uncountable 'metadata'
 end
