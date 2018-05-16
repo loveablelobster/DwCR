56fb3dfb0d222ccb2c10fa6031d24abcb08c01ff
Martin Stein <loveablelobster@fastmail.fm>
2018-05-16 09:46:14 +0200

    more elegant inflections

--- lib/dwcr/inflections.rb
+++ lib/dwcr/inflections.rb
@@ -1,19 +1,16 @@
 # frozen_string_literal: true
 
+INFLECTIONS = [
+  [:singular, /(media)$/i, '\1'],
+  [:plural, /(media)$/i, '\1'],
+  [:irregular, 'taxon', 'taxa'],
+  [:uncountable, 'metadata']
+].freeze
+
 Sequel.inflections do |inflect|
-#   inflect.uncountable 'multimedia'
-#   inflect.uncountable 'media'
-  inflect.singular(/(media)$/i, '\1')
-  inflect.plural(/(media)$/i, '\1')
-  inflect.irregular 'taxon', 'taxa'
-  inflect.uncountable 'metadata'
+  INFLECTIONS.each { |i| inflect.send(*i) }
 end
 
 String.inflections do |inflect|
-#   inflect.uncountable 'multimedia'
-#   inflect.uncountable 'media'
-  inflect.singular(/(media)$/i, '\1')
-  inflect.plural(/(media)$/i, '\1')
-  inflect.irregular 'taxon', 'taxa'
-  inflect.uncountable 'metadata'
+  INFLECTIONS.each { |i| inflect.send(*i) }
 end
